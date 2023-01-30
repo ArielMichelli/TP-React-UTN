c9b7da987ca7ca4cd6ef052b12ba226141c0698f
ArielMichelli <arielm-04@hotmail.com>
2023-01-30 14:37:53 -0300

    mejorando el Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <form action="">
-      <div className="row__form">
-      <label htmlFor="">Ingrese Usuario:</label>
-      <input type="text" name="usuario" id="user"/>
-      </div>
-      <div className="row__form">
-        <label htmlFor="">Password</label>
-        <input type="password" name="password" id="pass" />
-      </div>
-      <button className='button' type="submit">Ingresar</button>
-    </form>
+    <div className="contenedor__form">
+      <form action="">
+        <h1 className='title__form'>Bienvenido</h1>
+        <input type="text" name="usuario" id="user" placeholder='Usuario' />
+        <input type="password" name="password" id="pass" placeholder='Contraseña' />
+        <button className='button' type="submit">Ingresar</button>
+        <div className="text__form">
+          <p>¿Perdiste tu contraseña?</p>
+          <p>¿No tienes Cuenta? <a href="#">Registrarse</a></p>
+        </div>
+      </form>
+    </div>
   )
 }
 
