4922b901bc288b1e3e7636047a4fadfa5bc301f1
ArielMichelli <arielm-04@hotmail.com>
2023-02-23 10:34:21 -0300

    Agregando Bootstrap al navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Productos from './Services/Productos'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Registro from './pages/Registro';
-import Firebase from './Services/Firebase'; 
+import NavBarPro from './components/NavBar';
 
+//Firebase
+import Firebase from './Services/Firebase';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 //Routes:
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 
 function App() {
@@ -21,27 +20,20 @@ function App() {
   return (
 
     <div className="App">
-      
-      <Router>
-        <nav className="navbar">
-          <div className="navbar__der">
-            <Link to="/">Home</Link>
-            <Link to="/productos">Productos</Link>
-          </div>
-          <div className="navbar__izq">
-            <Link to="/login">Login</Link>
-            <Link to="/registro">Registro</Link>
-          </div>
-        </nav>
+      <>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos" element={<Productos />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<Registro />} />
-        </Routes >
-      </Router >
+        <BrowserRouter>
+          <NavBarPro />
 
+          <Routes>
+            <Route path="/" element={< Home />} />
+            <Route path="/productos" element={<Productos />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<Registro />} />
+            <Route path='*' element={<Navigate replace to="/" />} />
+          </Routes >
+        </BrowserRouter>
+      </>
     </div>
 
   )
