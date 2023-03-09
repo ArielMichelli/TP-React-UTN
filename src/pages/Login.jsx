88d60a34c52e63a0743081b32b4875b4c614c9c9
ArielMichelli <Arielm-04@hotmail.com>
2023-03-09 10:30:48 -0300

    Arreglando bug visuales

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,74 @@
-import React, { useState } from 'react'
-import firebase from '../Config/firebase';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import firebase from "../Config/firebase";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [logueado, setlogueado] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [logueado, setlogueado] = useState(false);
 
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await firebase.auth().signInWithEmailAndPassword(email, password);
+            setlogueado(true);
+            console.log(`Usuario Logeado Correctamente`);
+            alert("Usuario Logeado Correctamente");
+        } catch (error) {
+            console.log(`Error al loguear, Email/Password invalidos \n${error}`);
+            alert("Error al loguear, Email/Password invalidos");
+        }
+    };
+    const handleRecuperar = () => {
+        alert("Recuperacion de cuentas proximamente implementada...");
+    };
 
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    try {
-      await firebase.auth().signInWithEmailAndPassword(email, password)
-      setlogueado(true);
-      console.log(`Usuario Logeado Correctamente`);
-      alert("Usuario Logeado Correctamente");
-    } catch (error) {
-      console.log(`Error al loguear, Email/Password invalidos \n${error}`)
-    }
-  }
-  const handleRecuperar = () => {
-    alert("Recuperacion de cuentas proximamente implementada...")
-  }
+    return (
+        <>
+            <div className="contenedor__form">
+                <form>
+                    <h1 className="title__form">Bienvenido</h1>
+                    <input
+                        required
+                        type="email"
+                        name="email"
+                        id="email"
+                        onChange={(ev) => setEmail(ev.target.value)}
+                        placeholder="Correo Electronico"
+                    />
+                    <input
+                        required
+                        type="password"
+                        name="password"
+                        id="password"
+                        onChange={(ev) => setPassword(ev.target.value)}
+                        placeholder="Contraseña"
+                    />
 
-  return (
-    <>
-      <div className="contenedor__form">
+                    <button className="button" onClick={handleLogin}>
+                        Ingresar
+                    </button>
 
-        <form >
-          <h1 className='title__form'>Bienvenido</h1>
-          <input required type="email" name="email" id="email" onChange={(ev) => setEmail(ev.target.value)} placeholder='Correo Electronico' />
-          <input required type="password" name="password" id="password" onChange={(ev) => setPassword(ev.target.value)} placeholder='Contraseña' />
-
-          <button className='button' onClick={handleLogin}>Ingresar</button>
-
-          <div className="text__form">
-            <p>¿Perdiste tu contraseña? <a href="#" onClick={handleRecuperar}>Recuperar</a></p>
-            <p>¿No tienes Cuenta? <a href="#"><Link as={Link} to="/registro">Registrarse</Link></a></p>
-          </div>
-
-        </form>
-
-      </div>
-    </>
-  )
-}
+                    <div className="text__form">
+                        <p>
+                            ¿Perdiste tu contraseña?{" "}
+                            <a href="#" onClick={handleRecuperar}>
+                                Recuperar
+                            </a>
+                        </p>
+                        <p>
+                            ¿No tienes Cuenta?{" "}
+                            <a href="#">
+                                <Link as={Link} to="/registro">
+                                    Registrarse
+                                </Link>
+                            </a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
 
 export default Login;
